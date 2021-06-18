5f3939b63eee11cd864e3de8f425c153f7ac7723
Parth <tparth9531@gmail.com>
2021-06-19 00:08:35 +0530

    update

--- insert.php
+++ insert.php
@@ -1,32 +1,18 @@
 <?php  
 
-// Database configuration 
-$dbHost     = "localhost"; 
-$dbUsername = "root"; 
-$dbPassword = ""; 
-$dbName     = "php_one"; 
- 
-// Create database connection 
-$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName); 
-
-
- 
-// Check connection 
-if ($db->connect_error) { 
-    die("Connection failed: " . $db->connect_error); 
-}
-
+include 'db.php';
 
 extract($_POST);
 
-if (isset($first_name)) {
+if (isset($first_name))
+{
 	// code...
-
 
 	$sql = "INSERT INTO admin (first_name,last_name,mobile,email,password)
  	 VALUES ('$first_name','$last_name','$mobile','$email','$password')";
 
- 	 if (mysqli_query($db, $sql)) {
+ 	 if (mysqli_query($db, $sql)) 
+ 	 {
  		echo "New record created successfully !";
  	 } 
 }
